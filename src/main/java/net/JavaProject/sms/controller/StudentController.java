2cc07d39410c7e7eb38a8de9f85787ce875b30c9
Mohammad Sameer <mdsameer312398@gmail.com>
2024-01-23 08:21:19 +0530

    Making Fields Mandatory During dding Of Student details.

--- src/main/java/net/JavaProject/sms/controller/StudentController.java
+++ src/main/java/net/JavaProject/sms/controller/StudentController.java
@@ -29,13 +29,11 @@ public class StudentController {
         return "create_student";
     }
 
-
     @PostMapping("/students")
     public String saveStudent(@ModelAttribute("student")Student student){
         studentService.saveStudent(student);
         return "redirect:/students";
     }
-
 
     @GetMapping("/students/edit/{id}")
     public String editStudentForm(@PathVariable Long id, Model model){
